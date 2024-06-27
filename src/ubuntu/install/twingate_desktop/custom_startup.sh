7530d0801054f8efe45d7380cb2a3a17d525434a
chenbishop <chen.bishop@gmail.com>
2024-06-27 05:20:07 +0100

    testing

--- src/ubuntu/install/twingate_desktop/custom_startup.sh
+++ src/ubuntu/install/twingate_desktop/custom_startup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
+#START_COMMAND="google-chrome"
+#PGREP="chrome"
+#MAXIMIZE="true"
+DEFAULT_ARGS=""
+
 if [[ $MAXIMIZE == 'true' ]] ; then
     DEFAULT_ARGS+=" --start-maximized"
 fi
